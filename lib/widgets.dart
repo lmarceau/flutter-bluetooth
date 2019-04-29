2410f063fa7bd7556ab9cef8ec29058942729549
Laurie Marceau <laurie.marceau@carretechnologies.com>
2019-04-29 16:35:17 -0400

    Carousel a bit prettier

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -14,7 +14,8 @@ class Carousel extends StatelessWidget {
           body: _buildCarousel(context, scanResults)
       );
     } else {
-      return Text("Scan for devices");
+      // TODO: show scan for hexoskin devices image and text
+      return Text("");
     }
   }
 
@@ -26,18 +27,13 @@ class Carousel extends StatelessWidget {
         _buildCarouselTitle(context, scanResults),
         Spacer(),
         SizedBox(
-          height: 400.0,
+          height: 450.0,
           child: PageView.builder(
-            // TODO: store this controller in a State to save the carousel scroll position
-            controller: PageController(viewportFraction: 0.8),
+            controller: PageController(viewportFraction: 0.85),
             itemBuilder: (BuildContext context, int itemIndex) {
-              if (itemIndex < scanResults.length) {
-                return _buildCarouselItem(
-                    context, scanResults.elementAt(itemIndex), itemIndex);
-              } else {
-                return Text("");
-              }
+                return _buildCarouselItem(context, scanResults.elementAt(itemIndex), itemIndex);
             },
+            itemCount: scanResults.length,
           ),
         ),
         Spacer(flex: 5),
@@ -46,39 +42,82 @@ class Carousel extends StatelessWidget {
   }
 
   Widget _buildCarouselTitle(BuildContext context, List<ScanResult> scanResults) {
-    var title = scanResults.length == 1 ? "Hexoskin device" : "Hexoskin devices";
+    var title = scanResults.length == 1 ? "Found Hexoskin device" : "Found Hexoskin devices";
     return Text(title, style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold));
   }
 
   Widget _buildCarouselItem(BuildContext context, ScanResult result, int itemIndex) {
-    return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 4.0),
-        child: Stack(
-          alignment: Alignment.topCenter,
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: <Widget>[
+        _buildButtonBoxOutside(result),
+        _buildButtonBoxInside(),
+        Column(
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
-              ),
-            ),
-            Column(
-              children: <Widget>[
-                _buildTitle(context, result.device.name),
-                new Image.asset("assets/hx_device.png"),
-              ],
-            )
+            Spacer(flex: 4),
+            new Image.asset("assets/images/hx_device.png", height: 300),
+            Spacer(),
+            _buildButtonBoxTitle(result.device.name),
+            Spacer(flex: 2),
           ],
-        )
+        ),
+        InkWell(
+          onTap: () {
+              onTap();
+              print("Carousel was tapped with device: " + result.device.name);
+            },
+        ),
+      ],
     );
   }
 
-  Widget _buildTitle(BuildContext context, String deviceName) {
+  Widget _buildButtonBoxOutside(ScanResult result) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Colors.white10,
+        borderRadius: BorderRadius.circular(10.0),
+        shape: BoxShape.rectangle,
+        border: new Border.all(
+            color: Colors.grey,
+            width: 1.0,
+            style: BorderStyle.solid
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButtonBoxInside() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Colors.white10,
+        borderRadius: BorderRadius.circular(10.0),
+        shape: BoxShape.rectangle,
+        boxShadow: <BoxShadow>[
+          new BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 5.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtonBoxTitle(String deviceName) {
     return Column(
       children: <Widget>[
         Padding(
           padding: EdgeInsets.all(12.0),
-          child: Text(deviceName, style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
+          child: Text(
+            deviceName,
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
         )
       ],
     );
