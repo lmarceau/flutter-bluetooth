34489a9e300a0c65a9b9ed9d103ea933dc975ea5
Laurie Marceau <laurie.marceau@carretechnologies.com>
2019-04-30 17:56:06 -0400

    Getting ready on the device page. Next step => show the device data on the UI

--- lib/screens/device_page.dart
+++ lib/screens/device_page.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 import '../bluetooth.dart';
 
 class DevicePage extends StatelessWidget {
   DevicePage({Key key}) : super(key: key);
 
-//  _buildDeviceStateTile() {
-//    return new ListTile(
-//        leading: (deviceState == BluetoothDeviceState.connected)
-//            ? const Icon(Icons.bluetooth_connected)
-//            : const Icon(Icons.bluetooth_disabled),
-//        title: new Text('Device is ${deviceState.toString().split('.')[1]}'),
-//        subtitle: new Text('${device.name}'),
-//        trailing: new IconButton(
-//          icon: const Icon(Icons.refresh),
-//          onPressed: () => _refreshDeviceState(device),
-//          color: Theme.of(context).iconTheme.color.withOpacity(0.5),
-//        ));
-//  }
+  _buildDeviceStateTile(BuildContext context, Bluetooth model) {
+    return new ListTile(
+        leading: (model.deviceState == BluetoothDeviceState.connected)
+            ? const Icon(Icons.bluetooth_connected)
+            : const Icon(Icons.bluetooth_disabled),
+        title: new Text('Device is ${model.deviceState.toString().split('.')[1]}'),
+        subtitle: new Text('${model.device.name}'),
+    );
+  }
+
+  _buildActionButtons(Bluetooth model) {
+    if (model.isConnected) {
+      return <Widget>[
+        new IconButton(
+          icon: const Icon(Icons.cancel),
+          onPressed: () => model.disconnect(),
+        )
+      ];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<Bluetooth>(
       builder: (context, child, model) {
-        return new Container();
+        return new Scaffold(
+          appBar: new AppBar(
+            title: Text(model.device.name),
+            actions: _buildActionButtons(model),
+          ),
+          body: new Stack(
+            children: <Widget>[
+              _buildDeviceStateTile(context, model),
+            ]
+          )
+        );
       }
     );
   }
