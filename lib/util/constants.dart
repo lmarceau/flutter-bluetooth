bb5fc89b9929ffa45ec8b7184bfd263bb42bbb9f
Laurie Marceau <laurie.marceau@carretechnologies.com>
2019-04-30 17:51:33 -0400

    Adequate GATT constants

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,8 +1,13 @@
 
-/// Define Bluetooth Low Energy constants
-const String heartRateServiceUUID = "180D";
-const String heartRateMeasurementUUID = "2A37";
+/// Define GATT Bluetooth Low Energy constants
+const String batteryServiceUUID = "0000180f-0000-1000-8000-00805f9b34fb";
+const String batteryMeasurementUUID = "00002a19-0000-1000-8000-00805f9b34fb";
+
+const String heartRateServiceUUID = "0000180d-0000-1000-8000-00805f9b34fb";
+const String heartRateMeasurementUUID = "00002a37-0000-1000-8000-00805f9b34fb";
+
 const String respirationRateServiceUUID = "3b55c581-bc19-48f0-bd8c-b522796f8e24";
 const String respirationRateMeasurementUUID = "9bc730c3-8cc0-4d87-85bc-573d6304403c";
-const String accelerometerServiceUUID = "BDC750C7-2649-4FA8-ABE8-FBF25038CDA3";
-const String accelerometerMeasurementUUID = "75246A26-237A-4863-ACA6-09B639344F43";
+
+const String accelerometerServiceUUID = "bdc750c7-2649-4fa8-abe8-fbf25038cda3";
+const String accelerometerMeasurementUUID = "75246a26-237a-4863-aca6-09b639344f43";
